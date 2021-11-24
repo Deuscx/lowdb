4242f3fa1ebe48c0c11d0b9b1179460bce82fdec
deuscx <desucx@deusyue@gmail.com>
2021-11-24 08:34:10 +0800

    refactor: commonjs support

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+> This is a commonjs module for lowdb@3
 # lowdb [![](http://img.shields.io/npm/dm/lowdb.svg?style=flat)](https://www.npmjs.org/package/lowdb) [![Node.js CI](https://github.com/typicode/lowdb/actions/workflows/node.js.yml/badge.svg)](https://github.com/typicode/lowdb/actions/workflows/node.js.yml)
 
 > Simple to use local JSON database 🦉
---
+++ rollup.config.ts
@@ -0,0 +1,14 @@
+import { RollupOptions } from "rollup";
+import nodeResolve from '@rollup/plugin-node-resolve'
+import typescript from '@rollup/plugin-typescript'
+import { resolve} from 'path'
+
+const rsv = (path:string)=>resolve(__dirname,path)
+export default {
+    input: rsv('./src/index.ts'),
+    output:[{
+        format: 'cjs',
+        file: rsv('./lib/index.cjs.js')
+    }],
+    plugins:[nodeResolve(),typescript({ tsconfig: './tsconfig.json' })]
+} as RollupOptions
